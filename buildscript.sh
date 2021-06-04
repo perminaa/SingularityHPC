99821f9848326fefca16342088e48de97baebd90
Owen O'Connor <tux2603@gmail.com>
2021-06-04 16:26:29 -0400

    Clean up multipart commands

--- buildscript.sh
+++ buildscript.sh
@@ -17,50 +17,42 @@ echo 'Removing/Cleaning Up Previous Versions Complete'
 echo 'Installing Required Packages'
 
 # Installs essential packages
-sudo apt-get update && sudo apt-get install -y \
-	build-essential \
-	libssl-dev \
-	uuid-dev \
-	libgpgme11-dev \
-	squashfs-tools \
-	libseccomp-dev \
-	wget \
-	pkg-config \
-	git \
-	cryptsetup
+sudo apt-get update
+sudo apt-get install -y build-essential libssl-dev uuid-dev \
+                        libgpgme11-dev squashfs-tools libseccomp-dev \
+                        wget pkg-config git cryptsetup
 
 echo 'Installing Go'
 
 # Installs Go
-export VERSION=1.16.4 OS=linux ARCH=amd64 &&
-	wget https://dl.google.com/go/go$VERSION.$OS-$ARCH.tar.gz &&
-	sudo tar -C /usr/local -xzvf go$VERSION.$OS-$ARCH.tar.gz &&
-	rm go$VERSION.$OS-$ARCH.tar.gz
+export VERSION=1.16.4 OS=linux ARCH=amd64
+wget https://dl.google.com/go/go$VERSION.$OS-$ARCH.tar.gz
+sudo tar -C /usr/local -xzvf go$VERSION.$OS-$ARCH.tar.gz
+rm go$VERSION.$OS-$ARCH.tar.gz
 
-echo 'export PATH=/usr/local/go/bin:$PATH' >>~/.bashrc &&
-	source ~/.bashrc
+echo 'export PATH=/usr/local/go/bin:$PATH' >>~/.bashrc
+source ~/.bashrc
 
 echo 'Installing SingularityCE'
 
 # Installs Singularity
-export VERSION=3.7.3 && # adjust this as necessary \
-	wget https://github.com/hpcng/singularity/releases/download/v${VERSION}/singularity-${VERSION}.tar.gz &&
-	tar -xzf singularity-${VERSION}.tar.gz &&
-	cd singularity
+export VERSION=3.7.3
+wget https://github.com/hpcng/singularity/releases/download/v${VERSION}/singularity-${VERSION}.tar.gz
+tar -xzf singularity-${VERSION}.tar.gz
+rm singularity-${VERSION}}.tar.gz
+
+cd singularity
 
 echo 'Compiling the SingularityCE source code'
 
 # Compiles SingularityCE
-./mconfig &&
-	make -C builddir &&
-	sudo make -C builddir install
+./mconfig
+make -C builddir
+sudo make -C builddir install
 echo 'Installation Finished'
 
 # Switches to home directory
 cd ..
-# Removes any tar files
-
-rm singularity-*.gz
 
 # Runs the definition file
 singularity build vmdContainer.sif Singularity
